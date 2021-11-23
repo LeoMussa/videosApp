1759f1ca9409551a6ff74cd3635724f0338eeb08
Luis Mussa <Leomussalm@gmail.com>
2021-11-23 14:15:47 +0200

    Adicao do component SeachBar

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
 
 import './Tab1.css';
 
@@ -10,12 +10,14 @@ const Tab1: React.FC = () => {
           <IonTitle>Videos App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen color="light">
 
-      <img src="https://media-exp1.licdn.com/dms/image/C5603AQHXD9CUbewgDQ/profile-displayphoto-shrink_800_800/0/1625075990890?e=1643241600&v=beta&t=oNTbQm99tc_ncNi3J4sP_-h9Dx-lAccmfZCYucpfbiI" /> 
-       <h1> Luis Mussa</h1>
-       <p>Engenheiro de Software</p>
-       <a href="mailito:leomussalm@gmail.com">E-mail </a>
+
+      <IonContent fullscreen color="light">
+        <IonSearchbar placeholder="Pesquise o filme">
+
+        </IonSearchbar>
+
+      
      
       
       </IonContent>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 //import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import "./Tab3.css";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Sobre</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-    
+        <img src="https://media-exp1.licdn.com/dms/image/C5603AQHXD9CUbewgDQ/profile-displayphoto-shrink_800_800/0/1625075990890?e=1643241600&v=beta&t=oNTbQm99tc_ncNi3J4sP_-h9Dx-lAccmfZCYucpfbiI" />
+        <h1> Luis Mussa</h1>
+        <p>Engenheiro de Software</p>
+        <a href="mailito:leomussalm@gmail.com">E-mail </a>
       </IonContent>
     </IonPage>
   );
